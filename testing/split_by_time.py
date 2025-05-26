9009de6c333ab3c0edd6c0bcb1fc0c06048bf987
Tanmay Gejapati (asus) <tanmay4269@gmail.com>
2025-05-26 16:20:06 +0530

    train val test split works

--- testing/split_by_time.py
+++ testing/split_by_time.py
@@ -17,15 +17,8 @@ class OpenAlexGraphDataset:
         print("Done!")
 
         G = self._build_networkx_graph(json_path, max_num_nodes=num_authors)
-        node_features, node_to_idx = self._process_node_features(G) 
-        edge_indices, edge_features = self._process_edge_features(G, node_to_idx)
-
-        self.graph_data = Data(
-            x=node_features,
-            edge_index=edge_indices,
-            edge_attr=edge_features
-        )
-
+        self.train_data, self.val_data, self.test_data = self._create_splits(G)
+    
     def _build_networkx_graph(self, json_path, max_num_nodes):
         G = nx.Graph()
 
@@ -76,7 +69,7 @@ class OpenAlexGraphDataset:
         nodes_to_retain = [node for node in G.nodes() if node not in nodes_to_remove]
 
         return G.subgraph(nodes_to_retain).copy()
-    
+        
     def _process_node_features(self, G):
         max_citations = np.max([G.nodes[node_id]['citation_count'] for node_id in G.nodes()])
         institution_names = [G.nodes[node_id]['affiliated_institution'] for node_id in G.nodes()]
@@ -93,8 +86,8 @@ class OpenAlexGraphDataset:
         node_to_idx = {node: i for i, node in enumerate(G.nodes())}
         return node_features, node_to_idx
 
-    def _process_edge_features(self, G, node_to_idx):
-        edge_list = list(G.edges())
+    def _process_edge_features(self, G, node_to_idx, edges_to_include):
+        edge_list = [(u, v) for u, v in edges_to_include]
         mapped_edges = [[node_to_idx[u], node_to_idx[v]] for u, v in edge_list]
         edge_indices = torch.tensor(mapped_edges, dtype=torch.long).t().contiguous().to('cuda')
 
@@ -119,32 +112,83 @@ class OpenAlexGraphDataset:
         edge_features = torch.stack(edge_features_list)
         return edge_indices, edge_features
 
-    def get_data(self):
-        return self.graph_data
+    def _create_splits(self, G):
+        all_edges_with_dates = []
+        for u, v, data in G.edges(data=True):
+            for date in data['publication_dates']:
+                all_edges_with_dates.append(((u, v), date))
+
+        all_edges_with_dates.sort(key=lambda x: x[1])
+
+        num_edges = len(all_edges_with_dates)
+        train_end_idx = int(num_edges * 0.6)
+        val_end_idx = int(num_edges * 0.8)
+
+        train_edges_with_dates = all_edges_with_dates[:train_end_idx]
+        val_edges_with_dates = all_edges_with_dates[train_end_idx:val_end_idx]
+        test_edges_with_dates = all_edges_with_dates[val_end_idx:]
+
+        train_edges = list(set([edge for edge, _ in train_edges_with_dates]))
+        val_edges = list(set([edge for edge, _ in val_edges_with_dates]))
+        test_edges = list(set([edge for edge, _ in test_edges_with_dates]))
+
+        node_features, node_to_idx = self._process_node_features(G)
+
+        train_edge_indices, train_edge_features = self._process_edge_features(G, node_to_idx, train_edges)
+        val_edge_indices, val_edge_features = self._process_edge_features(G, node_to_idx, val_edges)
+        test_edge_indices, test_edge_features = self._process_edge_features(G, node_to_idx, test_edges)
+
+        train_data = Data(x=node_features, edge_index=train_edge_indices, edge_attr=train_edge_features)
+        val_data = Data(x=node_features, edge_index=val_edge_indices, edge_attr=val_edge_features)
+        test_data = Data(x=node_features, edge_index=test_edge_indices, edge_attr=test_edge_features)
+
+        return train_data, val_data, test_data
+
+    def get_train_data(self):
+        return self.train_data
+
+    def get_val_data(self):
+        return self.val_data
+
+    def get_test_data(self):
+        return self.test_data
 
 if __name__ == "__main__":
-    dataset_builder = OpenAlexGraphDataset(json_path="data/openalex_cs_papers.json")
-    graph_data = dataset_builder.get_data()
+    dataset_builder = OpenAlexGraphDataset(json_path="../data/openalex_cs_papers.json")
+    
+    train_graph_data = dataset_builder.get_train_data()
+    val_graph_data = dataset_builder.get_val_data()
+    test_graph_data = dataset_builder.get_test_data()
 
-    dataset = [graph_data]
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    print("---")
+    print("Train Dataset Information:")
+    print(f"Number of nodes: {train_graph_data.num_nodes}")
+    print(f"Number of edges: {train_graph_data.num_edges}")
+    print(f"Node feature shape: {train_graph_data.x.shape}")
+    print(f"Edge index shape: {train_graph_data.edge_index.shape}")
+    print(f"Edge attribute shape: {train_graph_data.edge_attr.shape}")
+    print(f"Node features device: {train_graph_data.x.device}")
+    print(f"Edge index device: {train_graph_data.edge_index.device}")
+    print(f"Edge attribute device: {train_graph_data.edge_attr.device}")
 
-    for batch in dataloader:
-        print(f"Type of batch object: {type(batch)}")
-        print(f"Batch content: {batch}")
+    print("---")
+    print("Validation Dataset Information:")
+    print(f"Number of nodes: {val_graph_data.num_nodes}")
+    print(f"Number of edges: {val_graph_data.num_edges}")
+    print(f"Node feature shape: {val_graph_data.x.shape}")
+    print(f"Edge index shape: {val_graph_data.edge_index.shape}")
+    print(f"Edge attribute shape: {val_graph_data.edge_attr.shape}")
+    print(f"Node features device: {val_graph_data.x.device}")
+    print(f"Edge index device: {val_graph_data.edge_index.device}")
+    print(f"Edge attribute device: {val_graph_data.edge_attr.device}")
 
-        print(f"\nNode features (x):")
-        print(f"  Shape: {batch.x.shape}")
-        print(f"  Device: {batch.x.device}")
-
-        print(f"\nEdge indices (edge_index):")
-        print(f"  Shape: {batch.edge_index.shape}")
-        print(f"  Device: {batch.edge_index.device}")
-
-        print(f"\nEdge features (edge_attr):")
-        print(f"  Shape: {batch.edge_attr.shape}")
-        print(f"  Device: {batch.edge_attr.device}")
-
-        print(f"\nNumber of nodes: {batch.num_nodes}")
-        print(f"Number of edges: {batch.num_edges}")
-        break
+    print("---")
+    print("Test Dataset Information:")
+    print(f"Number of nodes: {test_graph_data.num_nodes}")
+    print(f"Number of edges: {test_graph_data.num_edges}")
+    print(f"Node feature shape: {test_graph_data.x.shape}")
+    print(f"Edge index shape: {test_graph_data.edge_index.shape}")
+    print(f"Edge attribute shape: {test_graph_data.edge_attr.shape}")
+    print(f"Node features device: {test_graph_data.x.device}")
+    print(f"Edge index device: {test_graph_data.edge_index.device}")
+    print(f"Edge attribute device: {test_graph_data.edge_attr.device}")
