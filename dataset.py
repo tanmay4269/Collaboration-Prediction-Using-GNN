6664fb4343fae03c8490bb8737cfb5e12d1a312c
Tanmay Gejapati (asus) <tanmay4269@gmail.com>
2025-05-27 21:00:06 +0530

    dataset half way there

--- dataset.py
+++ dataset.py
@@ -71,11 +71,13 @@ class OpenAlexGraphDataset:
                 if author_id not in G:
                     G.add_node(
                         author_id,
+                        work_data=[(work["title"], publication_date)],
                         affiliated_institution=affiliation,
                         citation_count=cited_by
                     )
                 else:
                     G.nodes[author_id]["citation_count"] += cited_by
+                    G.nodes[author_id]["work_data"].append((work["title"], publication_date))
 
             for i in range(len(authors)):
                 for j in range(i + 1, len(authors)):
@@ -99,23 +101,47 @@ class OpenAlexGraphDataset:
 
         return G.subgraph(nodes_to_retain).copy()
         
-    def _process_node_features(self, G):
+    def _process_node_features(self, G, min_date, max_date):
         max_citations = np.max([G.nodes[node_id]['citation_count'] for node_id in G.nodes()])
-        institution_names = [G.nodes[node_id]['affiliated_institution'] for node_id in G.nodes()]
-        institution_embeddings = self.sentence_model.encode(institution_names, convert_to_tensor=True).to('cuda')
+        # institution_names = [G.nodes[node_id]['affiliated_institution'] for node_id in G.nodes()]
+        # institution_embeddings = self.sentence_model.encode(institution_names, convert_to_tensor=True).to('cuda')
+        
+        # all_work_data = [G.nodes[node_id]["work_data"] for node_id in G.nodes()]
+        filtered_author_titles = []
+        all_filtered_titles = []
+        for node_id in G.nodes():
+            data = G.nodes[node_id]["work_data"]
+            filtered_titles = []
+            for title, date in data:
+                if date >= min_date and date <= max_date:  # ! check if it works
+                    filtered_titles.append(title)
 
+            filtered_author_titles.append(filtered_titles)
+            all_filtered_titles.extend(filtered_titles)
+        
+        title_embeddings = self.sentence_model.encode(all_filtered_titles, convert_to_tensor=True).to('cuda')
+
+        i = 0
+        all_title_embeddings = []
+        for work_data in filtered_author_titles:
+            n = len(work_data)
+            all_title_embeddings.append(title_embeddings[i:i+n].mean(dim=1))  # ! check dim
+            i += n
+
+        assert len(all_title_embeddings) == len(G.nodes()), "Title embeddings' messed up"
+        
         node_features_list = []
         for i, node_id in enumerate(G.nodes()):
             node = G.nodes[node_id]
             scaled_citation_count = torch.tensor([node["citation_count"] / max_citations], dtype=torch.float).to('cuda')
-            feat = torch.cat((scaled_citation_count, institution_embeddings[i]))
+            feat = torch.cat([scaled_citation_count, all_title_embeddings[i]])
             node_features_list.append(feat)
-
+        
         node_features = torch.stack(node_features_list)
         node_to_idx = {node: i for i, node in enumerate(G.nodes())}
         return node_features, node_to_idx
 
-    def _process_edge_features(self, G, node_to_idx, edges_to_include):
+    def _process_edge_features(self, G, node_to_idx, edges_to_include, return_feats=False):
         edge_list = [(u, v) for u, v in edges_to_include]
         mapped_edges = [[node_to_idx[u], node_to_idx[v]] for u, v in edge_list]
         edge_indices = torch.tensor(mapped_edges, dtype=torch.long).t().contiguous().to('cuda')
@@ -129,6 +155,9 @@ class OpenAlexGraphDataset:
             title_slices.append((current_idx, current_idx + len(titles)))
             current_idx += len(titles)
 
+        if not return_feats:
+            return edge_indices
+        
         batched_title_embeddings = self.sentence_model.encode(all_individual_titles, convert_to_tensor=True).to('cuda')
 
         edge_features_list = []
@@ -141,7 +170,7 @@ class OpenAlexGraphDataset:
         edge_features = torch.stack(edge_features_list)
         return edge_indices, edge_features
 
-    def _create_splits(self, G):
+    def _create_splits(self, G, add_edge_attr=False):
         all_edges_with_dates = []
         for u, v, data in G.edges(data=True):
             for date in data['publication_dates']:
@@ -160,19 +189,25 @@ class OpenAlexGraphDataset:
         train_edges = list(set([edge for edge, _ in train_edges_with_dates]))
         val_edges = list(set([edge for edge, _ in val_edges_with_dates]))
         test_edges = list(set([edge for edge, _ in test_edges_with_dates]))
+        
+        last_train_date = train_edges_with_dates[-1][1]
+        last_val_date = val_edges_with_dates[-1][1]
 
-        node_features, node_to_idx = self._process_node_features(G)
+        if add_edge_attr:
+            node_features, node_to_idx = self._process_node_features(G)
 
-        train_edge_indices, train_edge_features = self._process_edge_features(G, node_to_idx, train_edges)
-        val_edge_indices, val_edge_features = self._process_edge_features(G, node_to_idx, val_edges)
-        test_edge_indices, test_edge_features = self._process_edge_features(G, node_to_idx, test_edges)
+            train_edge_indices, train_edge_features = self._process_edge_features(G, node_to_idx, train_edges)
+            val_edge_indices, val_edge_features = self._process_edge_features(G, node_to_idx, val_edges)
+            test_edge_indices, test_edge_features = self._process_edge_features(G, node_to_idx, test_edges)
 
-        train_data = Data(x=node_features, edge_index=train_edge_indices, edge_attr=train_edge_features)
-        val_data = Data(x=node_features, edge_index=val_edge_indices, edge_attr=val_edge_features)
-        test_data = Data(x=node_features, edge_index=test_edge_indices, edge_attr=test_edge_features)
+            train_data = Data(x=node_features, edge_index=train_edge_indices, edge_attr=train_edge_features)
+            val_data = Data(x=node_features, edge_index=val_edge_indices, edge_attr=val_edge_features)
+            test_data = Data(x=node_features, edge_index=test_edge_indices, edge_attr=test_edge_features)
+        else:
+            ...
 
         return train_data, val_data, test_data
-
+    
     def get_train_data(self):
         return self.train_data
 
